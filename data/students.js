fb30b61a23298241caeed9d8597b04db5499bf8d
Thomas Rice <ricethomasj@gmail.com>
2023-04-29 14:12:47 -0500

    adding functions to display cards. Adding forms. Add sort function

---
+++ data/students.js
@@ -0,0 +1,44 @@
+export const students = [
+  {
+    id: 1,
+    name: 'Harry Potter',
+    house: 'Ravenclaw',
+    gender: 'male',
+    avatar: '',
+  },
+  {
+    id: 2,
+    name: 'Hermoine Granger',
+    house: 'Slytherin',
+    gender: 'female',
+    avatar: '',
+  },
+  {
+    id: 3,
+    name: 'Ron Weasly',
+    house: 'Gryffindor',
+    gender: 'male',
+    avatar: '',
+  },
+  {
+    id: 4,
+    name: 'Draco Malfoy',
+    house: 'Hufflepuff',
+    gender: 'male',
+    avatar: '',
+  },
+  {
+    id: 5,
+    name: 'Luna Lovegood',
+    house: 'Gryffindor',
+    gender: 'female',
+    avatar: '',
+  },
+  {
+    id: 6,
+    name: 'Coolguy Coolson',
+    house: 'Ravenclaw',
+    gender: 'male',
+    avatar: '',
+  }
+]
